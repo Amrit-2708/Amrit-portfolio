71ea7ce4e82020e3a06f8d65df32354ac88dd66c
Amrit-2708 <rrajamrit.21@gmail.com>
2025-02-11 20:24:00 +0530

    create: Personal Portfolio # Created a personal portfolio to showcase my skills, projects and professional experiences.
    # The website is fully responsive and visually appealing which
    is created using HTML, CSS and JavaScript. Compatibility across
    different screen sizes and devices is ensured.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Personal_portfolio - Showcasing My Work and Skills
+
+## Live link : https://amrit-portfolio-seven.vercel.app/
+
+### -Designed and developed a personal portfolio website to showcase my skills, projects, and professional experiences.
+
+### -Crafted a responsive and visually appealing layout using HTML, CSS, and JavaScript, ensuring compatibility across different devices and screen sizes.
+
+##  Tech Stack: HTML, CSS, JavaScript
+
+
---
+++ script.js
@@ -0,0 +1,46 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    document.querySelector('.navbar').classList.toggle('active');
+});
+
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelectorAll('.navbar a').forEach(nav => nav.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const contactForm = document.getElementById('contact-form');
+
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        // Validate the form
+        const name = document.getElementById('name').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const message = document.getElementById('message').value.trim();
+
+        if (!name || !email || !message) {
+            alert('Please fill out all fields.');
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            alert('Please enter a valid email address.');
+            return;
+        }
+        
+        // Form data is valid, submit the form
+        contactForm.submit();
+
+        contactForm.reset();
+    });
+
+    function validateEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
+});
